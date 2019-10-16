d2e7fcd2d17efa6c51af8d60544a876583f48e50
Lex Bosch <law.bosch@student.han.nl>
2019-10-16 15:25:15 +0200

    creates workable somehting

--- jotihuntGroepenMapper.py
+++ jotihuntGroepenMapper.py
@@ -7,6 +7,53 @@
 
 import urllib.request
 import json
+import xml.etree.ElementTree as ET
+
+
+class create_kml_file:
+    def __init__(self):
+        self.colorlist = ["501400FF", "5014F0FF", "5000FF14", "507800F0", "501478E6", "50FFFFFF", "50000000"]
+        self.colorcounter = 0
+        self.style_list = []
+        self.full = ET.Element('kml')
+        self.full.set("xmlns", "http://www.opengis.net/kml/2.2")
+        self.docu = ET.SubElement(self.full, 'Document')
+        naam = ET.SubElement(self.docu, "name")
+        naam.text = "Jotihunt 2019 - Deelnemende Groepen"
+
+    def get_xml(self):
+        return ET.tostring(self.full)
+
+
+    def add_group(self, group_name, group_lon, group_lan, district):
+        group_mark = ET.SubElement(self.docu, "Placemark")
+        group_name_el = ET.SubElement(group_mark, "name")
+        group_name_el.text = group_name
+        group_style_el = ET.SubElement(group_mark, "styleUrl")
+        group_style_el.text = "#"+district.replace(".","") + "_style"
+        group_point_el = ET.SubElement(group_mark, "Point")
+        group_coord_el = ET.SubElement(group_point_el, "coordinates")
+        group_coord_el.text = "{0},{1},0".format(group_lon, group_lan)
+
+
+    def add_style(self, district):
+        if not district in self.style_list:
+            main_style = ET.SubElement(self.docu, "Style")
+            main_style.set("id",district.replace(".","")+"_style")
+            icon_style = ET.SubElement(main_style, "IconStyle")
+            ET.SubElement(icon_style, "color").text = self.colorlist[self.colorcounter]
+            self.colorcounter += 1
+            ET.SubElement(icon_style, "scale").text = "1"
+            subicon_style = ET.SubElement(icon_style, "Icon")
+            ET.SubElement(subicon_style, "href").text = "http://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png"
+            self.style_list.append(district)
+
+
+
+
+
+
+
 
 
 class Groups:
@@ -29,6 +76,10 @@ class Groups:
         """
         export_info = "{},{},{}".format(self.longtitute, self.latitute, self.group_name)
         return export_info
+
+    def get_better_location_info(self):
+        return self.group_name, self.longtitute, self.latitute, self.area
+
 
     def get_district(self):
         """Getting for group area
@@ -77,11 +128,17 @@ def create_csv(group_list):
     :param group_list: List containing group objects
     :return: returns string in csv format
     """
-    group_list = choose_district(group_list)
+#    group_list = choose_district(group_list)
     csv_file = "lon, lat, title\n"
+    kml_file = create_kml_file()
     for group in group_list:
-        csv_file += group.get_location_info() + "\n"
-    return csv_file
+        var1, var2, var3, var4 = group.get_better_location_info()
+        kml_file.add_group(var1, var2, var3, var4)
+        kml_file.add_style(var4)
+
+
+
+    return kml_file.get_xml().decode("utf-8")
 
 
 def choose_district(group_list):
@@ -124,7 +181,7 @@ def write_csv(csv_string):
     :param csv_string: string in csv format
     :return: csv file
     """
-    with open("jotihunt_location.csv", "w") as export_file:
+    with open("jotihunt_location.kml", "w") as export_file:
         export_file.write(csv_string)
 
 
